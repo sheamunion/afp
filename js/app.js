373da5d48a48a11b3e6063ea47a8f35ffaa80062
sheamunion <sheamunion@gmail.com>
2018-05-11 00:30:05 -0500

    Clean up syntax, rename styels.css.

--- js/app.js
+++ js/app.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
-	$('.graphic-photo-container span').click(function(){
+	function toggleClasses() {
 		$('.graphic-photo').toggleClass('graphic-blur');
 		$('.hide-on-click').toggle();
+	}
+
+	$('.graphic-photo-container span').click(function(){
+		toggleClasses();
 	});
 
 	$('.graphic-photo').click(function(){
-		$('.graphic-photo').toggleClass('graphic-blur');
-		$('.hide-on-click').toggle();
+		toggleClasses();
 	});
 });
